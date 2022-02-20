ce009219baa32e14ec442b83718770bf68183c14
antonio <antonioaugustopereiralima@gmail.com>
2022-02-20 09:40:34 -0300

    one more

--- 03 - Iniciando com Programação/11 - Desafio troca de valores copy.js
+++ 03 - Iniciando com Programação/11 - Desafio troca de valores copy.js
@@ -8,3 +8,12 @@ a = b;
 b = c;
 console.log("a =", a);
 console.log("b =", b);
+
+//Como trocar os valores das variáveis de uma forma mais fácil
+
+let d = 10;
+let e = 15;
+
+[d, e] = [e, d];
+console.log("d =", d);
+console.log("e =", e);

ab61b5db5fa7ab72ba591a475a9a52c98a6e2f35
antonio <antonioaugustopereiralima@gmail.com>
2022-02-19 22:10:34 -0300

    one more

---
+++ 03 - Iniciando com Programação/10 - Desafio Area da ciscunferencia.js
@@ -0,0 +1,9 @@
+//A = pi * r^2
+
+const PI = 3.1415;
+let raio = 10;
+
+let area = PI * raio * raio;
+
+console.log("A área de uma ciscunferência de raio", raio, "é", area, "metros quadrados");
+
---
+++ 03 - Iniciando com Programação/11 - Desafio troca de valores copy.js
@@ -0,0 +1,10 @@
+let a = 4;
+let b = 5;
+console.log("a =", a);
+console.log("b =", b);
+
+let c = a;
+a = b;
+b = c;
+console.log("a =", a);
+console.log("b =", b);

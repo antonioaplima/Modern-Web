de01b4e9a3fce2f2e0f5804a45fc9a963a7e3d4a
antonio <antonioaugustopereiralima@gmail.com>
2022-02-20 21:40:09 -0300

    Opa

---
+++ 04 - JavaScript_Fundamentos/05_Number_alguns_cuidados.js
@@ -0,0 +1,20 @@
+//Retorna infinito - Funciona 
+console.log(7/0);
+
+//Funciona 
+console.log("10"/5);
+
+//Não funciona 
+console.log("Show"*5);
+
+//Funciona com uma imprecisão 
+console.log(0.1+0.7);
+
+//console.log(10.toString); Não funciona
+
+//Funciona com o parênteses
+console.log((10).toString());
+
+
+
+

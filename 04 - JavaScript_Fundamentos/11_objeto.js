703b0459a6c784630e3795d7f642c8a20bac42c7
antonio <antonioaugustopereiralima@gmail.com>
2022-02-21 17:54:17 -0300

    crazy monday

--- 04 - JavaScript_Fundamentos/11_objeto.js
+++ 04 - JavaScript_Fundamentos/11_objeto.js
@@ -3,3 +3,21 @@ produto.nome = "Barbeador";
 produto.preco = 19.99;
 console.log(produto);
 
+//Mudando o valor da chave preco
+produto.preco = 1.99;
+console.log(produto);
+
+//Criando objetos dentro de objetos
+const produto2 ={
+    nome: "Faca",
+    preco: 20,
+    tipo: "Ferramenta",
+    obj: {
+        blabla: 1,
+        obj: {
+            blabla: 2
+        }
+    }
+}
+
+console.log(produto2);
---
+++ 04 - JavaScript_Fundamentos/12_null_undefined.js
@@ -0,0 +1,12 @@
+//Variável não inicializada
+let valor;
+console.log(valor);//undefined = nada foi atribuido para a variável
+
+valor = null;//Agora atribuimos o "vazio" para a variável
+console.log(valor);
+
+const produto = {};
+console.log(produto.preco);//preco ainda não foi definido -> undefined
+
+
+

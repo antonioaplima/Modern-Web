7d5f2941199da29f5fd38f0d2c8fb8cfa2b7fc02
antonio <antonioaugustopereiralima@gmail.com>
2022-02-24 23:09:52 -0300

    almost tgif

---
+++ 04 - JavaScript_Fundamentos/34_operadres_ternarios.js
@@ -0,0 +1,7 @@
+//Ternário: condição ? verdadeiro : falso
+const resultado = nota => (nota>=7 ? "Aprovado" : "Reprovado");
+
+console.log(resultado(8));
+console.log(resultado(7));
+console.log(resultado(6));
+

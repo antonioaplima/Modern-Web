47342a20fb6478fcc30b09c8013968650d6c4f3a
antonio <antonioaugustopereiralima@gmail.com>
2022-03-08 09:09:54 -0300

    esqueci de comitar ontem de noite

---
+++ 06 - Funcao/02_Params_variaveis.js
@@ -0,0 +1,13 @@
+//Não use isso :)
+function soma(){
+    let soma = 0;
+    for(i in arguments){
+        soma += arguments[i];
+    }
+    return soma;
+}
+
+console.log(soma());
+console.log(soma(1));
+console.log(soma(1, 2, 3));
+console.log(soma("Olhe que loucua", 1, 2, 3));

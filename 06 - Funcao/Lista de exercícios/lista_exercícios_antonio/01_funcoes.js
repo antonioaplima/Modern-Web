29cc50397adc9e6b3660946403c6d0b779830a54
antonio <antonioaugustopereiralima@gmail.com>
2022-03-15 00:02:14 -0300

    k

---
+++ 06 - Funcao/Lista de exercícios/lista_exercícios_antonio/01_funcoes.js
@@ -0,0 +1,11 @@
+const imprmirOperacoes = function(x = 0, y = 0){
+    console.log("Números:", x, "e", y);
+    console.log("Soma =", x + y);
+    console.log("Subtração =", x - y);
+    console.log("Produto =", x * y);
+    console.log("Quociente =", x / y);
+    console.log("-");
+}
+
+imprmirOperacoes();
+imprmirOperacoes(1, 2);

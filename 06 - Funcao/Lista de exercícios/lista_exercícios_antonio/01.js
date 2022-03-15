4cfcc061a199ea8bdc6c4b3f9a4efee0d00e55eb
antonio <antonioaugustopereiralima@gmail.com>
2022-03-14 23:46:52 -0300

    primeiro exercicio

---
+++ 06 - Funcao/Lista de exercícios/lista_exercícios_antonio/01.js
@@ -0,0 +1,11 @@
+const imprmirOperacoes = function(x = 1, y = 1) {
+    console.log("Numeros:", x, "e", y);
+    console.log("Adicao =", x + y);
+    console.log("Subtracao =", x - y);
+    console.log("Multiplicacao =", x * y);
+    console.log("Divisao =", x / y);
+}
+
+console.log(imprmirOperacoes(10, 5));
+
+

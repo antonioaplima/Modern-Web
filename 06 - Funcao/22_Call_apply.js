074aeb5bb723f4b780b03bd06f143267893121a1
antonio <antonioaugustopereiralima@gmail.com>
2022-03-14 23:36:42 -0300

    hmmm

--- 06 - Funcao/22_Call_apply.js
+++ 06 - Funcao/22_Call_apply.js
@@ -2,7 +2,7 @@ function getPreco(imposto = 0, moeda = "R$"){
     this.imposto = imposto;
     this.moeda = moeda;
 
-    return(`${moeda} ${this.preco * this.desconto * (1 + imposto)}`);
+    return(`${moeda} ${this.preco * (1 - this.desconto) * (1 + imposto)}`);
 }
 
 const produto = {
@@ -12,6 +12,17 @@ const produto = {
     getPreco
 }
 
-globalThis.preco = 20;
-globalThis.desconto = 0.3;
-console.log(getPreco());
+console.log(produto.nome);
+console.log(produto.getPreco());
+
+global.preco = 20;
+global.desconto = 0.1;
+console.log(getPreco());//usa o global como referencia
+
+const carro = {preco: 1000, desconto: 0.2}
+console.log(getPreco.call(carro));
+console.log(getPreco.apply(carro));
+
+console.log(getPreco.call(carro, 0.12, "$"));
+console.log(getPreco.apply(carro, [0.12, "$"]));
+
